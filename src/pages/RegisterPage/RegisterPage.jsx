c24fcd47b297b38065daaaafeef5da96e33a6e0c
Valentyn Digtiarenko <baggins999@gmail.com>
2022-07-01 18:37:14 +0300

    6.1

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import styles from './RegisterPage.module.css';
+import React from 'react';
+
 import { useDispatch } from 'react-redux';
 import { register } from '../../redux/auth/authOperations';
 
